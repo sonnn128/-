da3f66294848a588444c122a187fb37e25a9bf37
Nguyễn Ngọc Sơn <sonvipkl04@gmail.com>
2025-11-27 01:42:59 +0700

    .

--- src/main/java/com/sieuvjp/greenbook/GreenbookApplication.java
+++ src/main/java/com/sieuvjp/greenbook/GreenbookApplication.java
@@ -9,20 +9,4 @@ public class GreenbookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GreenbookApplication.class, args);
 	}
-
-	@org.springframework.context.annotation.Bean
-	public org.springframework.boot.CommandLineRunner run(com.sieuvjp.greenbook.service.UserService userService, org.springframework.security.crypto.password.PasswordEncoder passwordEncoder) {
-		return args -> {
-			if (!userService.existsByUsername("admin")) {
-				com.sieuvjp.greenbook.entity.User admin = new com.sieuvjp.greenbook.entity.User();
-				admin.setUsername("admin");
-				admin.setPassword(passwordEncoder.encode("admin"));
-				admin.setEmail("admin@greenbook.com");
-				admin.setRole(com.sieuvjp.greenbook.enums.Role.ADMIN);
-				admin.setActive(true);
-				userService.save(admin);
-				System.out.println("Admin account created: admin / admin");
-			}
-		};
-	}
 }
